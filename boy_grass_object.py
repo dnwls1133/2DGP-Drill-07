7fd983aca7a5641fce01b4be51d1ad69462bce1b
dnwls1133 <117172889+dnwls1133@users.noreply.github.com>
2025-09-30 16:33:03 +0900

    init commit 수업 내용한거 그대로 이어서 하시라 하셔서 그이후부터 커밋 들어가겠습니다.

---
+++ boy_grass_object.py
@@ -0,0 +1,95 @@
+from pico2d import *
+import random
+import random
+
+# Game object class here
+class Grass: # 클래스의 이름은 대문자로 하는것이 좋다.
+    def __init__(self):
+        self.image = load_image('grass.png')
+    def draw(self):
+        self.image.draw(400,30)
+
+    def update(self): pass
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100,700),90
+        self.frame = random.randint(0,7)
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame +1) % 8
+        self.x += 5
+        if(self.x > 800):
+            self.x = 0
+    def draw(self):
+        self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
+
+class Zombie:
+    def __init__(self):
+        self.x, self.y = 100, 170
+        self.frame = 0
+        self.image = load_image('zombie_run_animation.png')
+    def update(self):
+        self.frame = (self.frame+1) % 10
+        self.x += 5
+        if(self.x > 800):
+            self.x = 0
+    def draw(self):
+        frame_width = self.image.w // 10
+        frame_height = self.image.h
+        self.image.clip_draw(self.frame * frame_width,0, frame_width,
+                             frame_height,self.x,self.y, frame_width // 2, frame_height // 2)
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+
+
+def reset_world():
+    global running
+    global world
+    running = True
+    world = []
+
+    grass = Grass() # Grass 도장을 이용해서 grass 객체 생성
+    world.append(grass)
+
+    global team
+    team = [Boy() for i in range(10)]
+    world += team
+    zombie = Zombie()
+    world.append(zombie)
+
+def update_world():
+    for game_object in world:
+        game_object.update()
+
+    pass
+def render_world():
+    clear_canvas()
+    for game_object in world:
+        game_object.draw()
+    update_canvas()
+
+running = True
+
+open_canvas()
+
+reset_world()
+while running:
+    handle_events()
+    # 게임 로직
+    update_world()
+    render_world()
+    delay(0.05)
+
+
+close_canvas()
